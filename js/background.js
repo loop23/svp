7aa885aa7bf11dba85914906d088eb84fd89b744
Luca de Marinis <loop23@gmail.com>
2014-09-29 18:22:13 +0200

    Full screen invece di fixed size

--- js/background.js
+++ js/background.js
@@ -3,7 +3,7 @@
 chrome.app.runtime.onLaunched.addListener(function (arg) {
   chrome.app.window.create(
     'main.html',
-    { bounds: { left: 0, top: 0, width:1280, height:768 },
+    { state: 'fullscreen',
       frame:"none" },
   function(win) {
     win.maximize();
